186f74733cdee09173d3d520fe527591d1809bd0
shawn <jak47042007@163.com>
2015-01-19 22:09:42 -0800

    match 2 card

---
+++ Matchismo/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by shiyu wang on 1/19/15.
+//  Copyright (c) 2015 shawn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by shiyu wang on 1/19/15.
+//  Copyright (c) 2015 shawn. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
